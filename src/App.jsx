8a2a223f1ee7856104e81e53642d69ee0c8d34c2
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-04-14 00:20:40 +0600

    Complete Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
 
 function App() {
 
   return (
     <>
     <Navbar></Navbar>
+    <Footer></Footer>
     </>
   )
 }
