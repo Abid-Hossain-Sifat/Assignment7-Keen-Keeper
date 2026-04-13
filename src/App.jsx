4cd445a5420081dc2412518fbe753d15b912f4ac
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-04-13 22:23:52 +0600

    Complete Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
 import { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
-    <div>
-      <h1>
-        Hello world from main page
-
-      </h1>
-      <button className='btn btn-soft btn-primary'>Press here</button>
-    </div>
+    <Navbar></Navbar>
     </>
   )
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { NavLink } from 'react-router'
+import NavLogo from '../../../public/assets/logo.png'
+import { FaHome } from 'react-icons/fa'
+import { FaRegClock } from 'react-icons/fa'
+import { FaChartLine } from 'react-icons/fa'
+import './Navbar.css'
+const Navbar = () => {
+  return (
+    <div className='shadow-sm'>
+      <div className='max-w-[90%] mx-auto flex justify-between p-4.5 items-center'>
+        <div>
+            <img src={NavLogo} alt="" />
+        </div>
+        <div className='flex gap-1'>
+            <NavLink className='flex gap-1 items-center px-3 py-4 rounded-lg text-[#64748b]'><FaHome />Home</NavLink>
+            <NavLink className='flex gap-1 items-center px-3 py-4 rounded-lg text-[#64748b]'><FaRegClock />Timeline</NavLink>
+            <NavLink className='flex gap-1 items-center px-3 py-4 rounded-lg text-[#64748b]'><FaChartLine />State</NavLink>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
