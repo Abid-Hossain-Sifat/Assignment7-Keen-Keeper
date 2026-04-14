7e7ecdd9f05a1fae71e652aee281963fa59ca2b6
Abid-Hossain-Sifat <itzabid006@gmail.com>
2026-04-15 01:21:20 +0600

    added 404 error page

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Home from './Components/Home/Home.jsx';
 import Timeline from './Components/Timeline/Timeline.jsx';
 import Graph from './Components/StateGraph/Graph.jsx';
 import Details from './Components/Friends Details/Details.jsx';
+import Error404 from './Components/404 Error/Error.jsx';
 
 const router = createBrowserRouter([
   {
@@ -20,7 +21,8 @@ const router = createBrowserRouter([
       {index: true, Component: Home},
       {path: 'timeline', Component: Timeline},
       {path: 'state', Component: Graph},
-      {path: 'friend/:id', Component: Details}
+      {path: 'friend/:id', Component: Details},
+      {path: '*', Component: Error404}
     ]
   }
 ]);
